4f946bb8501567f6845964c6e22ec4f8da9fe323
Pradeeshwar <pradeeshxdev@gmail.com>
2023-03-30 02:25:19 +0530

    Add remvoe user button

--- blog-app/src/components/UserCard.vue
+++ blog-app/src/components/UserCard.vue
@@ -1,20 +1,28 @@
 <template>
-    <div class="d-flex mt-3 profile-card">
-        <router-link :to="'/profile/' + userDetails.u_id" style="width: 50%">
-            <div class="d-flex">
-                <div class="user-image">
-                    <img src="../assets/images/user.png" width="50px" height="50px" alt="user-image" />
+    <div>
+        <div class="d-flex mt-3 profile-card">
+            <router-link :to="'/profile/' + userDetails.u_id" style="width: 50%">
+                <div class="d-flex">
+                    <div class="user-image">
+                        <img src="../assets/images/user.png" width="50px" height="50px" alt="user-image" />
+                    </div>
+                    <div class="profile-name">
+                        <h6 class="m-0"><b>{{ userDetails.firstName + ' ' + userDetails.lastName }}</b></h6>
+                        <p class="m-0"><i>@{{ userDetails.username }}</i></p>
+                    </div>
                 </div>
-                <div class="profile-name">
-                    <h6 class="m-0"><b>{{ userDetails.firstName + ' ' + userDetails.lastName }}</b></h6>
-                    <p class="m-0"><i>@{{ userDetails.username }}</i></p>
-                </div>
+            </router-link>
+            <div>
+                <b-button v-if="!isRequestSent && !isFollowing" variant="outline-primary" @click="followUser"
+                    size="sm">Follow</b-button>
+                <b-button v-if="isRequestSent && !isFollowing" variant="outline-primary" @click="deleteFollowRequest"
+                    size="sm">Follow
+                    Request
+                    Sent!</b-button>
+                <b-button v-if="isRemoveButton && follower" variant="outline-danger" size="sm"
+                    @click="removeUserFromFollowing" class="mr-2">Remove User</b-button>
+                <b-button v-if="isFollowing" variant="outline-primary" @click="unfollowUser" size="sm">Unfollow</b-button>
             </div>
-        </router-link>
-        <div>
-            <b-button v-if="!isRequestSent" variant="outline-primary" @click="followUser" size="sm">Follow</b-button>
-            <b-button v-if="isRequestSent" variant="outline-primary" @click="deleteFollowRequest" size="sm">Follow Request
-                Sent!</b-button>
         </div>
     </div>
 </template>
@@ -23,11 +31,14 @@
 import axios from 'axios'
 export default {
     props: {
-        userDetails: Object
+        userDetails: Object,
+        follower: Boolean
     },
     data() {
         return {
-            isRequestSent: false
+            isRequestSent: false,
+            isFollowing: false,
+            isRemoveButton: false
         }
     },
     methods: {
@@ -37,6 +48,11 @@ export default {
                 receiver_id: this.userDetails.u_id
             }).then(() => {
                 this.isRequestSent = true
+                this.$bvToast.toast(`Follow Request Sent to @${this.userDetails.username}`, {
+                    title: 'Success',
+                    variant: 'success',
+                    solid: true
+                })
             }).catch(error => {
                 console.log(error)
             })
@@ -48,6 +64,11 @@ export default {
             }).then((response) => {
                 if (response.data.success == true) {
                     this.isRequestSent = false;
+                    this.$bvToast.toast(`Follow Request to @${this.userDetails.username} has been Removed!`, {
+                        title: 'Success',
+                        variant: 'danger',
+                        solid: true
+                    })
                 }
                 else {
                     this.$bvToast.toast('Something went wrong', {
@@ -69,10 +90,81 @@ export default {
                 console.log(err)
             })
         },
+
+        async checkIfFollowing() {
+            await axios.get(`http://localhost:5000/api/v1/is-following/${this.$store.state.user_id}/${this.userDetails.u_id}`,).then((res) => {
+                if (res.data.success == true) {
+                    this.isFollowing = true;
+                }
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+
+        async removeUser() {
+            await axios.get(`http://localhost:5000/api/v1/is-following/${this.userDetails.u_id}/${this.$store.state.user_id}`).then((res) => {
+                if (res.data.success == true) {
+                    this.isRemoveButton = true;
+                }
+            }).catch((err) => {
+                console.log(err)
+            })
+        },
+
+        async unfollowUser() {
+            await axios({
+                method: 'delete',
+                url: `http://localhost:5000/api/v1/unfollow/${this.$store.state.user_id}/${this.userDetails.u_id}`,
+            }).then((response) => {
+                if (response.data.success == true) {
+                    this.isFollowing = false;
+                    this.$bvToast.toast(`Unfollowed @${this.userDetails.username}`, {
+                        title: 'Success',
+                        variant: 'success',
+                        solid: true
+                    })
+                }
+                else {
+                    this.$bvToast.toast('Something went wrong', {
+                        title: 'Error',
+                        variant: 'danger',
+                        solid: true
+                    })
+                }
+            }).catch((error) => {
+                console.log(error);
+            })
+        },
+
+        async removeUserFromFollowing() {
+            await axios({
+                method: 'delete',
+                url: `http://localhost:5000/api/v1/remove-follower/${this.userDetails.u_id}/${this.$store.state.user_id}`,
+            }).then((response) => {
+                if (response.data.success == true) {
+                    this.$bvToast.toast(`@${this.userDetails.username} removed from your followers!`, {
+                        title: 'Success',
+                        variant: 'success',
+                        solid: true
+                    })
+                }
+                else {
+                    this.$bvToast.toast('Something went wrong', {
+                        title: 'Error',
+                        variant: 'danger',
+                        solid: true
+                    })
+                }
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
     },
 
     async created() {
         await this.checkPendingRequest()
+        await this.checkIfFollowing()
+        await this.removeUser()
     },
 
     watch: {
