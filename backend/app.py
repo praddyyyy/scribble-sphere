d1f41fb4d605b1210a11a2e4a261c03961609daa
Pradeeshwar <pradeeshxdev@gmail.com>
2023-04-09 13:37:03 +0530

    Add threading

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,9 @@ from service.scheduler import Scheduled
 import threading
 
 if __name__ == '__main__':
-    # t1 = threading.Thread(target=Scheduled)
-    # t1.start()
+    t1 = threading.Thread(target=Scheduled)
+    t1.start()
 
     app.run(debug=True)
+
+    t1.join()
